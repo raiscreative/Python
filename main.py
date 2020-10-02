ddcb92f7b1c6e7baab2579f3c2f34a3cb66b8b4f
raiscreative <64273721+raiscreative@users.noreply.github.com>
2020-10-02 12:03:17 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,14 @@
+'''
+This is about a song,
+ but is also about learning variables.
+'''
+
+Genre = 'Psychedelic rock' #String
+Artist = 'The Doors'
+Album = 'Strange Days'
+ReleaseYear = 1967 #Integer
+Released = 'September'  
+Recorded = 'May-August'
+BillboardPosition = 12
+DurationInSeconds = 132.0 #Float
+Songwriter1, Songwriter2 = 'Jim Morrison', 'Robbie Krieger'
